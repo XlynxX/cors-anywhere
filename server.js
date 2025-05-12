1559c4052e78195c0372ec0f2c9bc2095bdcc316
Kirils <47992209+XlynxX@users.noreply.github.com>
2025-05-12 21:40:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const http = require('http');
+// const http = require('http');
+// const httpProxy = require('http-proxy');
+
+// // Создаем прокси-сервер
+// const proxy = httpProxy.createProxyServer({});
+
+// http.createServer((req, res) => {
+//   // Прокси передает все заголовки и куки
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Это важно для отправки куков
+  
+//   // Перенаправляем запрос
+//   proxy.web(req, res, { target: 'https://teleopti.nordic.webhelp.com' }); 
+// }).listen(10000);
+
+const https = require('https');
 const httpProxy = require('http-proxy');
 
-// Создаем прокси-сервер
-const proxy = httpProxy.createProxyServer({});
+// Create custom agent that allows self-signed certificates
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
-http.createServer((req, res) => {
-  // Прокси передает все заголовки и куки
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Это важно для отправки куков
-  
-  // Перенаправляем запрос
-  proxy.web(req, res, { target: 'https://teleopti.nordic.webhelp.com' }); 
+httpProxy.createProxyServer({
+  target: 'https://teleopti.nordic.webhelp.com',  // Your target server
+  agent: agent,
 }).listen(10000);
