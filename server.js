8ee0b83a3dafe8be2db316cdc8a0e90cc69f24a2
Kirils Veremjovs <kveremjovs@gmail.com>
2025-05-22 14:11:09 +0300

    increase rate limit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // Rate Limiting: Allow 100 requests per 15 minutes
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 35, // Limit each IP to 100 requests per windowMs
+  max: 75, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests, please try again later.',
 });
 
