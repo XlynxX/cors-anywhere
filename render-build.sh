e131d2c3adc22c71c5452b109e1ab71721a81990
Kirils Veremjovs <kveremjovs@gmail.com>
2025-05-27 07:21:16 +0300

    changes render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -11,6 +11,9 @@ mkdir -p $PUPPETEER_CACHE_DIR
 # Install Puppeteer and download Chrome
 npx puppeteer browsers install chrome
 
+# Create the necessary cache directory for the build
+mkdir -p /opt/render/project/src/.cache/puppeteer/chrome/
+
 # Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
   echo "...Copying Puppeteer Cache from Build Cache"
